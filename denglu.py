f3558ff8e532c0d67fc8eea6d55eb50aca1284da
Archie1024 <13721330307@163.com>
2019-07-30 23:25:19 +0800

    7.30晚上的一些修改

--- denglu.py
+++ denglu.py
@@ -1,3 +1,12 @@
+
+""
+"""""--------------------------------一个获取名字的需求----------------------------------------------"""
+
+# 需求：写一个模拟登陆的小程序，用户名和密码以键值对的形式保存在字典中，若用户名输错，提示一直输入用户名；若用户名输对，提示
+# 输入对应的密码，密码只有三次输入机会，机会用完提示“账号锁定”，在每次输密错码后，提示今日剩余机会。
+
+
+
 # passwd = {"archie":"123","ajun":"2b"}
 # error_cnt = 3
 # userName = input("请输入用户名：").strip()
@@ -5,29 +14,30 @@
 #     #- 如果用户名错误，提示一直输入
 #     if userName not in passwd:
 #         userName  = input("请输入用户名：").strip()
-#         continue
-#     #- 用户名正确，提示输入密码
-#     psw = input("当前用户名是：{}，请输入密码：".format(userName).strip())
+#         continue  #contine的意思是不再运行下面的代码，继续上面的循环，只有不满足的时候，代码才向下运行。
+#     psw = input("当前用户名是：{}，请输入密码：".format(userName))
 #     #- 验证账号密码一一对应
 #     if (userName,psw) in passwd.items():
 #         print("欢迎用户：{},登陆成功！".format(userName))
 #         break#结束登陆
-#     else:#不成功，只要进来，一定是密码错误
+#     else:#不成功，只要进来，一定是密码错误，因为账号肯定是对的了。
 #         error_cnt -= 1
 #         if error_cnt ==0:#三次机会没有了
-#             print("今天三次机会没有了，请明天再试")
+#             print("今天已经输错三次，账号已锁定，请明天再试")
 #             break
 #         print("密码错误，您还有 {} 次机会".format(error_cnt))
 
 
 """--------------------------------一个获取名字的需求----------------------------------------------"""
+
+#根据日志信息获得纯净的用户名字
 """
 A old lady come in, the name is Mary,level 312
 A pretty boy come in,the name is Archie,level 32232432
 """
-# def get_name(srcstr):
-#     #函数体
-#     name = srcstr.split("is")[1].split(",")[0].strip()#以the name is 切开，下标取1，取到第二段,再以逗号切，取第一个，去空格
-#     return name
-# inStr = input("输入信息：")
-# print(get_name(inStr))
+def get_name(srcstr):
+    #函数体
+    name = srcstr.split("is")[1].split(",")[0].strip()#以the name is 切开，下标取1，取到第二段,再以逗号切，取第一个，去空格
+    return name
+inStr = input("输入信息：")
+print(get_name(inStr))
--- guifangdemo.py
+++ guifangdemo.py
@@ -1,25 +1,48 @@
-def binarySearch(nums,target):
-    print("数字列表总长度:",len(nums))
+"""
+有一个从小到大的数值列表，给定一个值X，使用二分法找出X在列表中的位置（下标）
+二分查找法：表中的元素是正序排列的，将表中间的值与X比较，若两者相等，则查找成功，否则利用中间位置纪录将表分成
+前后两个表，若中间的位置大于X，则查找前表，否则查找后表，重复以上操作，直到查找到结果或查找完毕所有数据。
+
+测试这段代码是否符合预期
+场景1、nums数量为奇数或偶数。测试结果：根据中间值坐标直接掐头去尾操作，奇偶无影响。
+场景2、猜想的数字为nums中有连续相等的数字，这种情况下取不到正常的值，所有用来排序高考成绩是不合适的！
+
+"""
+# def binarySearch(nums,target):
+#     print("数字列表总长度:",len(nums))
+#     start = 0
+#     end = len(nums) - 1
+#     while start <= end:
+#         print(nums[start:end + 1])   #披露每次掐头去尾后剩余nums的长度
+#         middle = (start + end) // 2
+#         print("start+end:{}+{}={}".format(start,end,(start+end)),"中间数下标：",middle,"中间数实际值:",nums[middle])
+#         if target > nums[middle]:
+#             start = middle + 1      #掐头操作
+#         elif target < nums[middle]:
+#             end = middle - 1   #去尾操作
+#         else:
+#             return middle      #刚好中间的那个数是
+#     return -1     #数字不在list里面！
+#
+# if __name__ == "__main__":
+#     List = [1,3,9,77,100,222,555,3333,44444]
+#     # print(List)
+#     print(binarySearch(List,77))
+
+def search_x(nums,x):
     start = 0
-    end = len(nums) - 1
+    end = len(nums)
     while start <= end:
-        print(nums[start:end + 1])   #披露每次掐头去尾后剩余nums的长度
-        middle = (start + end) // 2
-        print("start+end:{}+{}={}".format(start,end,(start+end)),"中间数下标：",middle,"中间数实际值:",nums[middle])
-        if target > nums[middle]:
-            start = middle + 1      #掐头操作
-        elif target < nums[middle]:
-            end = middle - 1   #去尾操作
+        middle = (start+end)//2
+        if  nums[middle] > x:  #(中间的都比X大，去尾操作)
+            end = middle - 1
+        elif nums[middle] < x:
+            start = middle + 1
         else:
-            return middle      #刚好中间的那个数是
-    return -1     #数字不在list里面！
+            return middle
+    return -1
 
 if __name__ == "__main__":
     List = [1,3,9,77,100,222,555,3333,44444]
     # print(List)
-    print(binarySearch(List,77))
-"""
-测试这段代码是否符合预期
-场景1、nums数量为奇数或偶数，根据中间值坐标直接掐头去尾操作，奇偶无影响
-场景2、猜想的数字为nums中有连续相等的数字，这种情况下取不到正常的值，所有用来排序高考成绩是不合适的！
-"""
+    print(search_x(List,444))
