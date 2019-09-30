cd43c0dd9a441008e86672d7f4bbc240cd254525
Archie1024 <52325401+Archie1024@users.noreply.github.com>
2019-09-30 17:03:34 +0800

    Add files via upload

--- chaku.py
+++ chaku.py
@@ -1,88 +1,89 @@
-# import sys
-# import MySQLdb
-#
-# userId = int(sys.argv[1])
-# planId = int(sys.argv[2])
-# seq = userId % 32
-#
-# host = "10.64.35.163"
-# port = 8823
-# user="test"
-# password="123456"
-#  12 db="danube"
-#  13
-#  14 danube = MySQLdb.connect(
-#  15     host=host,
-#  16     port=port,
-#  17     user=user,
-#  18     passwd=password,
-#  19     db=db)
-#  20
-#  21 curDanube = danube.cursor()
-#  22
-#  23 db="material"
-#  24
-#  25 material = MySQLdb.connect(
-#  26     host=host,
-#  27     port=port,
-#  28     user=user,
-#  29     passwd=password,
-#  30     db=db)
-#  31
-#  32 curMaterial = material.cursor()
-#  33
-#  34 db="creative"
-#  35
-#  36 creative = MySQLdb.connect(
-#  37     host=host,
-#  38     port=port,
-#  39     user=user,
-#  40     passwd=password,
-#  41     db=db)
-#  42
-#  43 curTemporary = creative.cursor()
-#  44
-#  45 print "----------------"
-#  46 print "danube_plan_info"
-#  47 print "----------------"
-#  48
-#  49 sql = "select * from danube_plan_info where id = " + str(planId)
-#  50 curDanube.execute(sql)
-#  51 for row in curDanube.fetchall():
-#  52     print " ".join([str(x) for x in row])
-#  53 sql = "select * from danube_creative_info_" + str(seq) + " where user_id = " + str(userId) + " and danube_plan_id = " + str(planId)
-#  54 curDanube.execute(sql)
-#  55 for row in curDanube.fetchall():
-#  56     mt = row[6]
-#  57     print "--------------------------------"
-#  58     print "danube_creative_info_" + str(seq) + " mt: " + str(mt)
-#  59     print "--------------------------------"
-#  60     print " ".join([str(x) for x in row])
-#  61     print "--------------------------------"
-#  62     print "formal_material_" + str(seq) + " mt: " + str(mt)
-#  63     print "--------------------------------"
-#  64     creative_id = row[3]
-#  65     sql = "select * from formal_material_" + str(seq) + " where id = " + str(creative_id)
-#  66     curMaterial.execute(sql)
-#  67     for row in curMaterial.fetchall():
-#  68         temporary_material_id = row[3]
-#  69         print " ".join([str(x) for x in row])
-#  70         print "-------------------------------"
-#  71         print "temporary_material_" + str(seq) + " mt: " + str(mt)
-#  72         print "-------------------------------"
-#  73         creative_id = row[3]
-#  74         sql = "select * from temporary_material_" + str(seq) + " where id = " + str(temporary_material_id)
-#  75         curTemporary.execute(sql)
-#  76         for row in curTemporary.fetchall():
-#  77             print " ".join([str(x) for x in row])
-#  78         print "--------------------------------"
-#  79         print "audit_info_" + str(seq) + " mt: " + str(mt)
-#  80         print "--------------------------------"
-#  81         creative_id = row[3]
-#  82         sql = "select * from audit_info_" + str(seq) + " where mc_id = " + str(temporary_material_id)
-#  83         curTemporary.execute(sql)
-#  84         for row in curTemporary.fetchall():
-#  85             print " ".join([str(x) for x in row])
-#  86 curTemporary.close()
-#  87 curMaterial.close()
-#  curDanube.close()
+import sys
+import mysqlclient.MySQLdb
+
+userId = int(sys.argv[1])
+planId = int(sys.argv[2])
+seq = userId % 32
+
+host = "10.64.35.163"
+port = 8823
+user="test"
+password="123456"
+db="danube"
+
+danube = MySQLdb.connect(
+    host=host,
+    port=port,
+    user=user,
+    passwd=password,
+    db=db)
+
+curDanube = danube.cursor()
+
+db="material"
+
+material = MySQLdb.connect(
+    host=host,
+    port=port,
+    user=user,
+    passwd=password,
+    db=db)
+
+curMaterial = material.cursor()
+
+db="creative"
+
+creative = MySQLdb.connect(
+    host=host,
+    port=port,
+    user=user,
+    passwd=password,
+    db=db)
+
+curTemporary = creative.cursor()
+
+print("----------------------------------------------")
+print("danube_plan_info")
+print("----------------")
+
+sql = "select * from danube_plan_info where id = " + str(planId)      #plan的各种信息
+curDanube.execute(sql)
+for row in curDanube.fetchall():
+    print(" ".join([str(x) for x in row])) #join用法再看看，好像平时用的少哦。把列表变成了字符串。
+
+sql = "select * from danube_creative_info_" + str(seq) + " where user_id = " + str(userId) + " and danube_plan_id = " + str(planId)
+curDanube.execute(sql)   #各种新建信息
+for row in curDanube.fetchall():
+    mt = row[6]
+    print("--------------------------------")
+    print("danube_creative_info_" + str(seq) + " mt: " + str(mt))
+    print("--------------------------------")
+    print(" ".join([str(x) for x in row]))
+    print("--------------------------------")
+    print("formal_material_" + str(seq) + " mt: " + str(mt))
+    print("--------------------------------")
+    creative_id = row[3]   #这里好像应该是row4
+    sql = "select * from formal_material_" + str(seq) + " where id = " + str(creative_id)
+    curMaterial.execute(sql)
+    for row in curMaterial.fetchall():
+        temporary_material_id = row[3]
+        print(" ".join([str(x) for x in row]))
+        print("-------------------------------")
+        print("temporary_material_" + str(seq) + " mt: " + str(mt))
+        print("-------------------------------")
+        creative_id = row[3]
+        sql = "select * from temporary_material_" + str(seq) + " where id = " + str(temporary_material_id)
+        curTemporary.execute(sql)
+        for row in curTemporary.fetchall():
+            print(" ".join([str(x) for x in row]))
+        print("--------------------------------")
+        print("audit_info_" + str(seq) + " mt: " + str(mt))
+        print("--------------------------------")
+        creative_id = row[3]
+        sql = "select * from audit_info_" + str(seq) + " where mc_id = " + str(temporary_material_id)
+        curTemporary.execute(sql)
+        for row in curTemporary.fetchall():
+            print(" ".join([str(x) for x in row]))
+curTemporary.close()
+curMaterial.close()
+curDanube.close()
